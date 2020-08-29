31904faa11aff3dec5670af0e5c40d3240caad22
AntonChetveryk <chetverykanton92@gmail.com>
2020-08-29 18:40:55 +0300

    Updates

---
+++ precache-manifest.d8b0bf5579551e5b3d07e674ce5be2a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da535631c5f233039bc8f214bf272bf4",
+    "url": "/swapi-tm/index.html"
+  },
+  {
+    "revision": "c38869e36f74d90d5314",
+    "url": "/swapi-tm/static/css/main.07a718d4.chunk.css"
+  },
+  {
+    "revision": "f9b1ea4f23d4e9538214",
+    "url": "/swapi-tm/static/js/2.1a11e500.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/swapi-tm/static/js/2.1a11e500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38869e36f74d90d5314",
+    "url": "/swapi-tm/static/js/main.796feb9d.chunk.js"
+  },
+  {
+    "revision": "54dc210b6575ae9ceaca",
+    "url": "/swapi-tm/static/js/runtime-main.262fc4e3.js"
+  }
+]);
